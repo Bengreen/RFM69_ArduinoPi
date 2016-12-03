0c5dc80c7521bc74f865e7214fc6b50c2c553177
Ben Greene <b.greene@analyticsengines.com>
2016-12-03 19:24:52 +0000

    Auto based build system working

--- syncToPi.sh
+++ syncToPi.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
-(cd .. ; rsync -rv RFM69_ArduinoPi dietpi: )
+(cd .. ; rsync --exclude='.git' --exclude ".DS_Store" --exclude ".git*" -rv RFM69_ArduinoPi dietpi: )
+
+ssh dietpi "(cd RFM69_ArduinoPi; aclocal; autoheader; automake --add-missing; autoconf; ./configure; make)"
